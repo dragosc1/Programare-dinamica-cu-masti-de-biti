77b7b70df872d243d334145a3fc39742bceae7f8
dragosc1 <dragos.ciobanu232@gmail.com>
2025-06-14 22:31:57 +0300

    Uploaded capitolul 6

---
+++ Capitolul 6/Grid_Gradient.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fast ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+ 
+int dp[2][(1 << 20)];
+ 
+const int MOD = 1e9 + 7;
+ 
+int main() {
+    fast
+    // citire date de intrare
+    int n, m;
+    cin >> n >> m;
+    
+    // Numarul de griduri gradient
+    dp[0][0] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int mask = 0; mask < (1 << m); mask++) {
+                if (!(mask & (1 << j))) {
+                    dp[1][mask] = (dp[1][mask] + dp[0][mask]) % MOD;
+                }
+                if (!(mask & (1 << j)) && (j == 0 || !(mask & (1 << (j - 1))))) {
+                    int new_mask = mask ^ (1 << j);
+                    dp[1][new_mask] = (dp[1][new_mask] + dp[0][mask]) % MOD;
+                }
+                if (mask & (1 << j)) {
+                    int new_mask = mask ^ (1 << j);
+                    dp[1][new_mask] = (dp[1][new_mask] + dp[0][mask]) % MOD;
+                }
+            }
+            for (int mask = 0; mask < (1 << m); mask++) {
+                dp[0][mask] = dp[1][mask];
+                dp[1][mask] = 0;
+            }
+        }
+    }
+    
+    int result = 0;
+    for (int mask = 0; mask < (1 << m); mask++) {
+        result = (result + dp[0][mask]) % MOD;
+    }
+
+    // afisarea rezultatului
+    result = result * 2 % MOD; // prima celula poate fi numar par sau impar
+    cout << result << endl;
+    return 0;
+}
---
+++ Capitolul 6/Problema_domino.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fast ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+ 
+int dp[2][(1 << 10)];
+ 
+const int MOD = 1e9 + 7;
+ 
+int main() {
+    fast
+    // citire date de intrare
+    int N, M;
+    cin >> N >> M;
+    swap(N, M);
+
+    // dp[0][k] = numarul de moduri de a plasa domino-uri pe o tabla de dimensiune n x m
+    dp[0][0] = 1;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j < M; j++) {
+            for (int k = 0; k < (1 << M); k++) {
+                dp[1][k ^ (1 << j)] = (dp[1][k ^ (1 << j)] + dp[0][k]) % MOD;
+                if (j != M - 1 && !(k & (1 << j)) && !(k & (1 << (j + 1)))) {
+                    dp[1][k ^ (1 << (j + 1))] =
+                        (dp[1][k ^ (1 << (j + 1))] + dp[0][k]) % MOD;
+                }
+            }
+            for (int k = 0; k < (1 << M); k++) 
+                dp[0][k] = dp[1][k], dp[1][k] = 0;
+        }
+    }
+
+    // afisarea rezultatului
+    int rezultat = dp[0][0];
+    cout << rezultat << '\n';
+    return 0;
+}
